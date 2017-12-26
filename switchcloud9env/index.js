bba770dee2d6dbc4d268544aca3e2e430975f9aa
takedai0313 <takedai@live.jp>
2017-12-26 18:42:15 +0000

    first commit

---
+++ switchcloud9env/index.js
@@ -0,0 +1,41 @@
+'use strict';
+
+const fs = require("fs")
+const AWS = require('aws-sdk');
+const cloud9 = new AWS.Cloud9({
+    apiVersion: '2017-09-23',
+    region: 'ap-southeast-1'
+});
+
+exports.handler = (event, context, callback) => {
+    const env = JSON.parse(fs.readFileSync(process.env.cloud9Env, 'utf8'));
+    const envList = env.envList;
+    const accountNo = env.accountNo;
+    const lang = event.lang;
+    
+    Object.keys(envList).forEach((key) => {
+        if(lang === key){
+            const inputparm = {
+                environmentId: `${envList[key]}`,
+                permissions: 'read-write',
+                userArn: `arn:aws:iam::${accountNo}:user/${event.name}`
+            };
+            cloud9.createEnvironmentMembership(inputparm, (err, data) => {
+               // ユーザの有無に関わらず登録(既にいるならエラーになるだけ)
+               if (err) console.log(err, err.stack);
+               else     console.log("success to create membership"); 
+            });
+        }else{
+            const inputparm = {
+                environmentId: `${envList[key]}`,
+                userArn: `arn:aws:iam::${accountNo}:user/${event.name}`
+            };
+            cloud9.deleteEnvironmentMembership(inputparm, (err, data) => {
+                // ユーザの有無に関わらずメンバシップ削除(いないならエラーになるだけ)
+               if (err) console.log(err, err.stack);
+               else     console.log("success to delete membership");
+            });
+        }
+    });
+    callback();
+};
